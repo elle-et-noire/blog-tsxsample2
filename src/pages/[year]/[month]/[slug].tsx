d4214efe51acd40c8d1d21889b88078907ad7525
L Lawliet <atibon3@gmail.com>
2022-04-27 19:38:05 +0900

    favicon test

--- src/pages/[year]/[month]/[slug].tsx
+++ src/pages/[year]/[month]/[slug].tsx
@@ -70,7 +70,8 @@ export default function View(props: Props) {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@L48610" />
         <link rel="shortcut icon"
-          href="https://user-images.githubusercontent.com/51241098/139574778-08c0d89e-b88e-4faa-baf5-4ee2af007b81.png"
+          // href="https://user-images.githubusercontent.com/51241098/139574778-08c0d89e-b88e-4faa-baf5-4ee2af007b81.png"
+          href="/images/L.png"
           type="image/png" />
       </Head>
       <article>
